2d878a4f0d20bc1130d8e113e0de1a2deccc238d
Adilso Alteia <adilso.alteia.jr@gmail.com>
2022-02-26 11:19:16 -0300

    Listar Contas Commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,45 @@
-import classes.Cliente;
-import classes.Conta;
-import classes.ContaCorrente;
-import classes.ContaPoupanca;
+import classes.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //Cria banco e lista de contas;
+        Banco banco1 = new Banco("Banco do Brasil");
+        List<Conta> contasB1 = new ArrayList<>();
+        banco1.setListContas(contasB1);
+
+        //Cria clientes
         Cliente c1 = new Cliente();
+        Cliente c2 = new Cliente();
+        Cliente c3 = new Cliente();
+        Cliente c4 = new Cliente();
         c1.setNome("Adilso");
+        c2.setNome("Luiz");
+        c3.setNome("Alteia");
+        c4.setNome("Junior");
 
-        Conta cc = new ContaCorrente(c1);
-        Conta poupanca = new ContaPoupanca(c1);
+        //Cria Contas
+        Conta cc1 = new ContaCorrente(c1);
+        Conta poup1 = new ContaPoupanca(c1);
 
-        cc.depositar(100);
-        cc.transferir(poupanca, 27.56);
+        Conta cc2 = new ContaCorrente(c2);
 
-        cc.imprimirExtrato();
-        poupanca.imprimirExtrato();
+        Conta cc3 = new ContaCorrente(c3);
+        Conta poup3 = new ContaPoupanca(c3);
+
+        Conta poup4 = new ContaPoupanca(c4);
+
+        banco1.adicionarConta(cc1);
+        banco1.adicionarConta(poup1);
+        banco1.adicionarConta(cc2);
+        banco1.adicionarConta(cc3);
+        banco1.adicionarConta(poup3);
+        banco1.adicionarConta(poup4);
+
+        banco1.listarContas();
+
 
     }
 }
--- src/classes/Banco.java
+++ src/classes/Banco.java
@@ -6,11 +6,25 @@ public class Banco {
     private String nome;
     private List<Conta> listContas;
 
+    public void adicionarConta(Conta conta){
+        this.listContas.add(conta);
+    }
+
+    public void listarContas(){
+        for (int i = 0; i < this.listContas.size(); i++) {
+            listContas.get(i).imprimirExtrato();
+        }
+    }
+
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Banco(String nome) {
         this.nome = nome;
     }
 
